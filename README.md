f686ccd8620317386beb3522a6e89cdfda7e1e4a
gaoxingliang <gaoxingliang@outlook.com>
2022-09-23 10:18:13 +0800

    init impl of java.util.BitSet

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# java-collection-in-python
-The java collections in python impl
+# The java collections in python implementation
+Now it includes [BitSet](https://docs.oracle.com/javase/7/docs/api/java/util/BitSet.html)
+
+
+# how to use?
+see [github](https://github.com/gaoxingliang/java-collection-in-python)
+
+
---
+++ src/example.py
@@ -0,0 +1,21 @@
+
+from javaCollectionInPython.bitset import BitSet
+
+bs = BitSet.valueOf([])
+
+bs.set(123, True)
+bs.set(456, True)
+# how many bits set?
+print(bs.cardinality())
+
+
+# bitset operations:
+bs2 = BitSet.valueOf([])
+bs2.set(223, True)
+bs2.set(456, True)
+# logical operations like: or_ xor, and andNot etc
+bs.or_(bs2)
+print(bs.cardinality())
+# check a value exists or not
+print(bs.get(123))
+print(bs.get(7))
